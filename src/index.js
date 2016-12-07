3a7ea1208555d0e4636e1ba07c2ec67f899e81fc
k-kinzal <log@logn.in>
2016-12-08 01:14:11 +0900

    add flowtype

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
+/* @flow */
 'use strict';
 
-exports.handler = () => {
-  console.log('Hello World!!');
+/*::
+type LambdaEvent = {
+  key1?: ?string;
+  key2?: ?string;
+  key3?: ?string;
+};
+type LambdaContext = {
+
+};
+type LambdaCallback = (error: ?Error, result: ?Object) => void;
+*/
+
+exports.handler = (event/*: LambdaEvent */, context/*: LambdaContext */, callback/*: LambdaCallback */) => {
+  callback(null, {success: true});
 };
